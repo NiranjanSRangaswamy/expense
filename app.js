c1108070132713e632aea1d151b5245ec6090175
Niranjan S.R <108211452+NiranjanSRangaswamy@users.noreply.github.com>
2024-02-08 13:47:20 +0530

    cors update

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const Router = require("./routers");
+const cors = require('cors');
 dotenv.config({ path: "./.env" });
 const app = express();
 
 const dbURI = process.env.DATABASE;
 const port = process.env.PORT;
 
+app.use(cors({origin:'*'}));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
